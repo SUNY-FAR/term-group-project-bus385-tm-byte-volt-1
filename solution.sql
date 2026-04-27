6d48b16ebd5aa7e5d106ae4eecb2cf3c87204e03
tm-byte-volt <tarism@farmingdale.edu>
2026-04-26 22:00:13 -0400

    Update solution.sql
    Signed-off-by: tm-byte-volt <tarism@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,8 +1,1 @@
-SELECT 
-    LEFT(make_and_model, INSTR(make_and_model, ' ') - 1) AS Brand,
-    COUNT(vehicles.vehicle_id) AS `Number of Vehicles`
-FROM vehicles
-INNER JOIN services 
-    ON vehicles.vehicle_id = services.vehicle_id
-GROUP BY LEFT(make_and_model, INSTR(make_and_model, ' ') - 1)
-ORDER BY Brand;
+SELECT * FROM users;
