53e2bb92ab89b4917760b04b0a7727fdd08454c9
tm-byte-volt <tarism@farmingdale.edu>
2026-04-26 21:06:51 -0400

    Service Totals By Brand.sql
    Signed-off-by: tm-byte-volt <tarism@farmingdale.edu>

--- solution.sql
+++ solution.sql
@@ -1,16 +1,8 @@
--- Write your SQL query below:
-
 SELECT 
-    LEFT(Make_and_Model, POSITION(' ' IN Make_and_Model) - 1) AS Brand,
-    COUNT(VEHICLES.Vehicle_ID) AS Number_of_Vehicles,
-
-    COUNT(CASE WHEN Service_Type = 'Oil Change' THEN 1 END) AS Oil_Change,
-    COUNT(CASE WHEN Service_Type = 'Brake Repair' THEN 1 END) AS Brake_Repair,
-    COUNT(CASE WHEN Service_Type = 'Towing' THEN 1 END) AS Towing
-
-FROM VEHICLES
-JOIN SERVICES 
-    ON VEHICLES.Vehicle_ID = SERVICES.Vehicle_ID
-
-GROUP BY Brand
+    LEFT(make_and_model, INSTR(make_and_model, ' ') - 1) AS Brand,
+    COUNT(vehicles.vehicle_id) AS `Number of Vehicles`
+FROM vehicles
+INNER JOIN services 
+    ON vehicles.vehicle_id = services.vehicle_id
+GROUP BY LEFT(make_and_model, INSTR(make_and_model, ' ') - 1)
 ORDER BY Brand;
